09cee52f5ac16ac2e2122edfb5be60bb57fd2810
Niels Olesen <no@tec.dk>
2024-05-22 11:35:37 +0200

    22. maj 2024 [2]

--- H4SoftwareTest/Codes/EncryptionHandler.cs
+++ H4SoftwareTest/Codes/EncryptionHandler.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Security.Cryptography;
+using System.Text;
 
 namespace H4SoftwareTest.Codes;
 
@@ -32,32 +33,40 @@ public class EncryptionHandler
 
     #region Asymetric encryption
 
-    public async Task<string> EncryptAsymetriscParent(string txtToEncrypt)
-    {
-        string[] data = new string[2] { txtToEncrypt, _publicKey };
-        string serializedValue = JsonConvert.SerializeObject(data);
-        StringContent content = new StringContent(serializedValue, System.Text.Encoding.UTF8, "application/json");
-        var response = await _httpClient.PostAsync("https://localhost:7171/api/Encrypt", content);
-        string encryptedValue = await response.Content.ReadAsStringAsync();
-        return encryptedValue;
-    }
-
-    //public string EncryptAsymetrisc(string txtToEncrypt)
+    //public async Task<string> EncryptAsymetriscParent(string txtToEncrypt)
     //{
-    //    string txtToEncrypt = value[0];
-    //    string publicKey = value[1];
-
-    //    RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
-    //    rsa.FromXmlString(publicKey);
-
-    //    byte[] txtToEncryptAsByteArry = System.Text.Encoding.UTF8.GetBytes(txtToEncrypt);
-    //    byte[] encryptedValue = rsa.Encrypt(txtToEncryptAsByteArry, true);
-    //    string encryptedValueAsString = Convert.ToBase64String(encryptedValue);
-
-    //    return encryptedValueAsString;
+    //    string[] data = new string[2] { txtToEncrypt, _publicKey };
+    //    string serializedValue = JsonConvert.SerializeObject(data);
+    //    StringContent content = new StringContent(serializedValue, System.Text.Encoding.UTF8, "application/json");
+    //    var response = await _httpClient.PostAsync("https://localhost:7171/api/Encrypt", content);
+    //    string encryptedValue = await response.Content.ReadAsStringAsync();
+    //    return encryptedValue;
     //}
 
+    // Dette kryptering kode er det som ligger i web api:
+    public string EncryptAsymetrisc(string txtToEncrypt)
+    {
+        RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+        rsa.FromXmlString(_publicKey);
 
+        byte[] txtToEncryptAsByteArry = System.Text.Encoding.UTF8.GetBytes(txtToEncrypt);
+        byte[] encryptedValue = rsa.Encrypt(txtToEncryptAsByteArry, true);
+        string encryptedValueAsString = Convert.ToBase64String(encryptedValue);
+
+        return encryptedValueAsString;
+    }
+
+    public string DecryptAsymetrisc(string txtToDecrypt)
+    {
+        RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+        rsa.FromXmlString(_privateKey);
+
+        byte[] txtToDecryptAsByteArry = Convert.FromBase64String(txtToDecrypt); // System.Text.Encoding.UTF8.GetBytes(txtToDecrypt);
+        byte[] encryptedValue = rsa.Decrypt(txtToDecryptAsByteArry, true);
+        string encryptedValueAsString = System.Text.Encoding.UTF8.GetString(encryptedValue);  // Convert.ToBase64String(encryptedValue);
+
+        return encryptedValueAsString;
+    }
 
     #endregion
 }
